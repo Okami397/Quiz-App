05c5094786fe489b495596ffd8c166a0131b566a
Okami397 <aard.wolf@list.ru>
2024-08-07 16:35:36 +0500

    Add basic statistics page

--- Quiz App/src/pages/Statistics/Statistics.tsx
+++ Quiz App/src/pages/Statistics/Statistics.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Statistics = () => {
-  return <div></div>;
+  return (
+    <div>
+      <p>Number of questions</p>
+      <p>Number of correct answers</p>
+      <p>Number of questions for each category that user took</p>
+      <p>Number of questions for each difficulty</p>
+      <p>Number of questions for each type</p>
+    </div>
+  );
 };
 
 export default Statistics;
