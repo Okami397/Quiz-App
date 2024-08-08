8f1ed51c65a79313443c6ea6054b83bdac3ccfe7
Okami397 <aard.wolf@list.ru>
2024-08-08 13:09:42 +0500

    update depenencies, fixes quizResult spent time

--- Quiz App/src/pages/QuizResult/QuizResult.tsx
+++ Quiz App/src/pages/QuizResult/QuizResult.tsx
@@ -56,7 +56,7 @@ const QuizResult: React.FC<QuizResultProps> = () => {
           />
           <div className={styles.wrapper__results_time}>
             <h3>Time spent:</h3>
-            <p>{correct.totalQuestions} second's</p>
+            <p>{correct.time} second's</p>
           </div>
         </section>
         <section className={styles.container__card}>
