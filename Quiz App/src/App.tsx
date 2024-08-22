47d7524339bb0d9626fdbc040584daa197a8e499
Okami397 <aard.wolf@list.ru>
2024-08-22 14:06:59 +0500

    app router refactoring

--- Quiz App/src/App.tsx
+++ Quiz App/src/App.tsx
@@ -6,40 +6,19 @@ import QuizResult from "./pages/QuizResult/QuizResult";
 import Statistics from "./pages/Statistics/Statistics";
 import PageTransition from "./components/UI/spinners/PageTransition";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <PageTransition>
-        <QuizConfig />
-      </PageTransition>
-    ),
-  },
-  {
-    path: "/quiz",
-    element: (
-      <PageTransition>
-        <MainQuiz />
-      </PageTransition>
-    ),
-  },
-  {
-    path: "/result",
-    element: (
-      <PageTransition>
-        <QuizResult />
-      </PageTransition>
-    ),
-  },
-  {
-    path: "/statistics",
-    element: (
-      <PageTransition>
-        <Statistics />
-      </PageTransition>
-    ),
-  },
-]);
+const routes = [
+  { path: "/", component: QuizConfig },
+  { path: "/quiz", component: MainQuiz },
+  { path: "/result", component: QuizResult },
+  { path: "/statistics", component: Statistics },
+];
+
+const refinedRoutes = routes.map(({ path, component }) => ({
+  path,
+  element: <PageTransition>{React.createElement(component)}</PageTransition>,
+}));
+
+const router = createBrowserRouter(refinedRoutes);
 
 const App: React.FC = () => {
   return (
